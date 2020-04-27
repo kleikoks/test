db5b79d328f3e3867831de2ab861b4d54de7479a
kleikoks <kleikoks.py@agmail.ru>
2020-04-27 22:08:55 +0300

    ad

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 І грім із небес пролунав надо головами грішників, і задзвеніли колокольчики твоєї мамаші, чо???
 Точно нема що пушити?????????????????
 Ля ну ти само нарвалось!!!!!!!!!!!!!!!!!
+git pullllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
